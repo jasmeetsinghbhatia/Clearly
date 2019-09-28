152d978938dc8e477f4238e693c12541ad224eb0
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2019-09-28 18:58:10 -0400

    More CSS changes

---
+++ static/js/recipe.js
@@ -0,0 +1,37 @@
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+get_meal_btn.addEventListener('click', () => {
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(res => res.json())
+		.then(res => {
+		createMeal(res.meals[0]);
+	}).catch(e => {
+		console.warn(e);
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
